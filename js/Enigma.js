d8ede540b7c5cab25407c716e14652dbe5e699d8
LiTsungYi <litsungyi@gmail.com>
2015-04-24 04:53:53 +0000

    Initial

---
+++ js/Enigma.js
@@ -0,0 +1,16 @@
+var rotors = new Array( 5 );
+rotors[ 0 ] = 2;
+rotors[ 1 ] = 5;
+rotors[ 2 ] = 11;
+rotors[ 3 ] = 17;
+rotors[ 4 ] = 23;
+
+
+
+
+window.onload = function()
+{
+    document.getElementById( "Rotor" ).onclick = function;
+    
+    
+};
